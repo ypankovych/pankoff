785591c0be6092cbce40e3a920a6e3be56ed6cdb
yaroslavpankovych <flower.moor@gmail.com>
2021-05-15 01:20:01 +0300

    bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-version = "3.0"
+version = "4.0"
 setup(
     name='pankoff',  # How you named your package folder (MyLib)
     packages=['pankoff'],  # Chose the same as "name"
