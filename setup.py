958a397515433831bf63add94c392f2e80deaffc
yaroslavpankovych <flower.moor@gmail.com>
2021-05-15 03:00:07 +0300

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,21 @@ from distutils.core import setup
 
 version = "6.0"
 setup(
-    name='pankoff',  # How you named your package folder (MyLib)
-    packages=['pankoff'],  # Chose the same as "name"
-    version=version,  # Start with a small number and increase it with every change you make
-    license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description='Easy fields validator',  # Give a short description about your library
-    author='Yaroslav Pankovych',  # Type in your name
-    author_email='flower.moor@gmail.com',  # Type in your E-Mail
-    url='https://github.com/ypankovych/pankoff',  # Provide either the link to your github or to your website
-    download_url=f'https://github.com/ypankovych/pankoff/archive/refs/tags/{version}.tar.gz',  # I explain this later on
-    keywords=['validation', 'easy', 'pure python'],  # Keywords that define your package best
+    name='pankoff',
+    packages=['pankoff'],
+    version=version,
+    license='MIT',
+    description='Easy fields validator',
+    author='Yaroslav Pankovych',
+    author_email='flower.moor@gmail.com',
+    url='https://github.com/ypankovych/pankoff',
+    download_url=f'https://github.com/ypankovych/pankoff/archive/refs/tags/{version}.tar.gz',
+    keywords=['validation', 'easy', 'pure python'],
     classifiers=[
         'Development Status :: 3 - Alpha',
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
